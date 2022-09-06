fd01bd924c1bea17abbabbf1b17f9fe0358d1b96
Aan060606 <saan05224@gmail.com>
2022-09-06 20:42:31 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const username = "Aan";
+const password = "aan123";
+const pin = 123;
+
+let num = 1232;
+var data = {nama: "Aan"};
+
+console.log("Username : " + data.nama + "\nPassword : " + password);``
